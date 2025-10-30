fa3da7e584fd364454a77823d00780915c69f918
Roheem <76229253+Abdur-roheem@users.noreply.github.com>
2025-10-30 20:56:55 +0000

    first_stage

---
+++ deployment/predict.py
@@ -0,0 +1,30 @@
+import pickle
+import requests
+import uvicorn
+
+from fastapi import FastAPI
+# from flask import Flask, request, jsonify 
+from typing import Dict, Any
+
+app = FastAPI(title = "lead-scorer")
+
+with open('pipeline_v1.bin', 'rb') as f_in:
+    pipeline = pickle.load(f_in)
+
+
+def predict_single(customer):
+    result = pipeline.predict_proba(customer)[0, 1]
+    return float(result)
+
+
+@app.post("/predict")
+def predict(customer: Dict[str, Any]):
+    prob = predict_single(customer)
+
+    return {
+        "score_probability": prob,
+        "score": bool(prob >= 0.5)
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9696)
